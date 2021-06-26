d0fea1c696a59846e288b09e148036c1eebd9b74
tugumeandree <andrewtugume2@gmail.com>
2021-06-27 00:37:38 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -4,45 +4,45 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>qraftacademy</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Qraft <a href="qraftacademy.org">Academy</a>
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+         COMING SOON
         </p>
 
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="#" className="card">
+            <h3>Apprenticeship &rarr;</h3>
+            <p>Work Opportunities.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href="#" className="card">
+            <h3>Founders Design Sprint &rarr;</h3>
+            <p>Self Employment</p>
           </a>
 
           <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="#"
             className="card"
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>Career Training And Development Events &rarr;</h3>
+            <p>Community Development</p>
+          </a>
+
+          <a
+            href="#"
+            className="card"
+          >
+            <h3>Consultancy &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Corporate and schools Training, development, guidance and counselling
             </p>
           </a>
         </div>
@@ -50,12 +50,12 @@ export default function Home() {
 
       <footer>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
+          qraftacademy 2021
+        
         </a>
       </footer>
 
